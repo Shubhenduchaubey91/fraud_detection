4121ccbe566ad34d101cb97d4fa22062d169a5ab
shubhenduchaubey91 <shubhenduchaubey91@gmail.com>
2024-02-07 16:13:28 +0530

    new code for model

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,12 @@
 print("adding all ML related code here")
+
+
+def model_1():
+    print("code for ML model 1")
+
+def model_2():
+    print("code for ML model 2")
+
+
+def model_3():
+    print("code for ML model 3")
